06d2b86588bae9e501bdb6cba3f920769d94fdfe
brucehan <intragerhan@gmail.com>
2025-11-28 23:15:36 +0900

    초난감 DAO

---
+++ src/main/java/springbook/user/dao/NUserDao.java
@@ -0,0 +1,15 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class NUserDao extends UserDao {
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        // N 사 DB Connection 생성코드
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost/springbook", "root", "cometrue"
+        );
+    }
+}
