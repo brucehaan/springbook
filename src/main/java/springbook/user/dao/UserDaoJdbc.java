c4c6eee523b32217e03aad41d503dc229e3df325
brucehan <intragerhan@gmail.com>
2025-12-16 17:37:07 +0900

    예외 전환

--- src/main/java/springbook/user/dao/UserDaoJdbc.java
+++ src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -1,6 +1,7 @@
 package springbook.user.dao;
 
-import com.mysql.cj.exceptions.MysqlErrorNumbers;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
@@ -10,7 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-public class UserDao {
+public class UserDaoJdbc implements UserDao {
 
     private JdbcTemplate jdbcTemplate;
 
@@ -30,29 +31,30 @@ public class UserDao {
                 }
             };
 
-    public void add(final User user) throws DuplicateUserIdException {
-        try {
+    public void add(final User user) throws DataAccessException {
+//        try {
             this.jdbcTemplate.update("insert into users(id, name, password) values(?, ?, ?)",
                     user.getId(), user.getName(), user.getPassword());
-        } catch (SQLException e) {
-            // ErrorCode가 MySQL의 "Duplicate Entry(1062)"이면 예외 전환
-            if (e.getErrorCode() == MysqlErrorNumbers.ER_DUP_ENTRY)
-                throw new DuplicateUserIdException(e); // 예외 전환
-            else
-                throw new RuntimeException(e); // 예외 포장
-        }
+//        } catch (DataAccessException e) {
+//            // ErrorCode가 MySQL의 "Duplicate Entry(1062)"이면 예외 전환
+////            if (e.getErrorCode() == MysqlErrorNumbers.ER_DUP_ENTRY)
+////                throw new DuplicateUserIdException(e); // 예외 전환
+////            else
+////                throw new RuntimeException(e); // 예외 포장
+//            throw new DuplicateUserIdException(e); // 예외 전환
+//        }
     }
 
-    public User get(String id) throws SQLException {
+    public User get(String id) {
         return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id},
                 this.userMapper);
     }
 
-    public void deleteAll() throws SQLException {
+    public void deleteAll() {
         this.jdbcTemplate.update("delete from users");
     }
 
-    public int getCount() throws SQLException {
+    public int getCount() {
         return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
     }
 
--- src/test/java/springbook/user/UserDaoTest.java
+++ src/test/java/springbook/user/UserDaoTest.java
@@ -2,16 +2,15 @@ package springbook.user;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import springbook.user.dao.JdbcContext;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
 import springbook.user.dao.UserDao;
+import springbook.user.dao.UserDaoJdbc;
 import springbook.user.domain.User;
 
 
@@ -21,13 +20,15 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 //@ExtendWith(SpringExtension.class)
 //@ContextConfiguration(locations = "/test-applicationContext.xml")
 //@DirtiesContext // 테스트 메소드에서 애플리케이션 컨텍스트의 구성이나 상태를 변경한다는 것을 테스트 컨텍스트 프레임워크에 알려준다
 public class UserDaoTest {
 
-    UserDao dao; // UserDao 타입 빈을 직접 DI 받는다
+    private UserDaoJdbc dao; // UserDao 타입 빈을 직접 DI 받는다
+    private DataSource dataSource;
 
     private User user1;
     private User user2;
@@ -42,12 +43,11 @@ public class UserDaoTest {
 
         // 오브젝트 생성, 관계설정 등을 모두 직접 해준다
         // 애플리케이션 컨텍스트가 만들어지는 번거로움은 없어졌지만, 매번 새로운 테스트 오브젝트를 만들어야 하는 단점이 있다
-        dao = new UserDao();
-        DataSource dataSource = new SingleConnectionDataSource( // 테스트에서 UserDao가 사용할 DataSource 오브젝트를 직접 생성한다.
+        dao = new UserDaoJdbc();
+        dataSource = new SingleConnectionDataSource( // 테스트에서 UserDao가 사용할 DataSource 오브젝트를 직접 생성한다.
                 "jdbc:mysql://localhost:3306/springbook", "root", "cometrue", true
         );
-        dao.setDataSource(dataSource); // 코드에 의한 수동 DI
-
+        dao.setDataSource(this.dataSource);
         // 테스트용 DataSource로 초기화한 JdbcContext를 직접 생성
         // dao.setJdbcContext(jdbcContext)로 주입하도록 구성해, 수동 DI 상황에서도 jdbcContext 필드가 null이 되지 않도록 함.
 //        JdbcContext jdbcContext = new JdbcContext();
@@ -130,5 +130,21 @@ public class UserDaoTest {
         assertThat(user1.getPassword()).isEqualTo(user2.getPassword());
     }
 
-
+    @Test
+    void duplicateKey() {
+        dao.deleteAll();
+//
+//        assertThrows(DataAccessException.class, () -> {
+//            dao.add(user1);
+//            dao.add(user1);
+//        });
+        try {
+            dao.add(user1);
+            dao.add(user1);
+        } catch (DuplicateKeyException ex) {
+            SQLException sqlEx = (SQLException) ex.getRootCause();
+            SQLExceptionTranslator translator = new SQLErrorCodeSQLExceptionTranslator(this.dataSource);
+            assertThat(translator.translate(null, null, sqlEx)).isInstanceOf(DuplicateKeyException.class);
+        }
+    }
 }
